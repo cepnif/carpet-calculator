b2686f0d1cba1862cbfbc1b666ce082f0060787f
Fatih Cepni <fatih@fincityltd.com>
2020-12-23 14:26:04 +0000

    areas calculated

---
+++ carpet-calculator/src/main/java/com/fincityltd/services/floor/Kitchen.java
@@ -0,0 +1,19 @@
+package com.fincityltd.services.floor;
+
+import com.fincityltd.interfaces.floorTypes.Floor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class Kitchen implements Floor {
+
+    @Value("${diameter}")
+    BigDecimal radius;
+
+    @Override
+    public BigDecimal getArea() {
+        return radius.multiply(radius).multiply(new BigDecimal(Math.PI));
+    }
+}
---
+++ carpet-calculator/src/main/java/com/fincityltd/services/floor/LivingRoom.java
@@ -0,0 +1,21 @@
+package com.fincityltd.services.floor;
+
+import com.fincityltd.interfaces.floorTypes.Floor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class LivingRoom implements Floor {
+
+    @Value("${length}")
+    BigDecimal length;
+    @Value("${width}")
+    BigDecimal width;
+
+    @Override
+    public BigDecimal getArea() {
+        return length.multiply(width);
+    }
+}
